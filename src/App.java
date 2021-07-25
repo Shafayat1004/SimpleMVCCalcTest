362a21f6761a00e0fdd48362359a18bafa386f9b
Mir Shafayat Ahmed <shafayat1004@live.com>
2021-07-26 01:09:13 +0600

    added

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+        
     }
 }
