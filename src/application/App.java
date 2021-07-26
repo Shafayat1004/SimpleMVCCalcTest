9e9bba787499ab09cf9f100989a36e4921459f6e
Mir Shafayat Ahmed <shafayat1004@live.com>
2021-07-26 14:27:27 +0600

    avoided class javafx.scene.Scene cannot be cast to class javafx.scene.Parent by removing parent root

--- src/application/App.java
+++ src/application/App.java
@@ -1,29 +1,25 @@
 package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application{
-    public static void main(String[] args) throws Exception {
-        launch(args);
-    }
 
     @Override
     public void start(Stage stage) throws Exception {
         try{
-            Parent root = FXMLLoader.load(getClass().getResource("CalcView.fxml"));
-            Scene scene = new Scene(root, 720,720);
+            //Parent root = FXMLLoader.load(getClass().getResource("CalcView.fxml"));            
             // scene.getStylesheets().add(getClass().getResource("application.css"));
-            Image icon = new Image("icon.png");
-            stage.getIcons().add(icon);
-            stage.setTitle("calculator");
+            Scene scene = FXMLLoader.load(getClass().getResource("CalcView.fxml"));
             stage.setScene(scene);
             stage.show();
         }catch(Exception e){
             e.printStackTrace();
         }
     }
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
 }
